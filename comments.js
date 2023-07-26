604d9917e89750ea38bcfc5b6c052c1d9a87fe0e
SimplyReal <111626458+SimplyReal@users.noreply.github.com>
2023-07-26 17:13:02 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,36 +1,12 @@
 //create web server
 const express = require('express');
-const readline = require('readline-sync');
-const path = require('path');
-
 const app = express();
+const port = 8000;
 
-// Function to get the desired port number from the user
-function getPortFromUser() {
-  const defaultPort = 8000; // Default port if no input is provided
-  const portInput = readline.question(`Enter the port number (default: ${defaultPort}): `);
-
-  const port = parseInt(portInput, 10);
-
-  if (isNaN(port) || port <= 0 || port > 65535) {
-    console.log('Invalid port number. Using the default port.');
-    return defaultPort;
-  }
-
-  return port;
-}
-
-const port = getPortFromUser();
-
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Define a route for the default page
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send('Hello, World!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
